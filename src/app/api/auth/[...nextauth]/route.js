87b27ac470b238e77dac84893f1f293d964afdde
Ankit Kumain <kumankit10@gmail.com>
2024-03-05 22:17:37 +0530

    feat: add sign in with linkedin

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import LinkedInProvider from "next-auth/providers/linkedin";
+
+export const authOptions = {
+  providers: [
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      authorization: { params: { scope: "profile email openid" } },
+      issuer: "https://www.linkedin.com",
+      jwks_endpoint: "https://www.linkedin.com/oauth/openid/jwks",
+      async profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          firstname: profile.given_name,
+          lastname: profile.family_name,
+          email: profile.email,
+        };
+      },
+    }),
+  ],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
