b3833cb6d143284d3c7bb8f7db115ffee146a0f5
Ankit Kumain <kumankit10@gmail.com>
2024-03-06 21:15:42 +0530

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 "use client";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { data } = useSession();
-  console.log(data);
+  const router = useRouter();
+
+  if (data?.user) {
+    return router.push("/roast-profile");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      <div className="h-screen flex items-center justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => signIn()}
+        >
+          Sign in with Linkedin
+        </button>
+      </div>
+    </main>
   );
 }
