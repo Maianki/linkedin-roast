24a7ef7f7a8e79c24c5d413fbb8a0b7b191dba0a
Ankit Kumain <kumankit10@gmail.com>
2024-03-07 22:29:40 +0530

    migrate: next-auth to kinde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
-"use client";
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
-  const { data } = useSession();
-  const router = useRouter();
-
-  if (data?.user) {
-    return router.push("/roast-profile");
-  }
-
   return (
-    <main className="">
-      <div className="h-screen flex items-center justify-center">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => signIn()}
-        >
-          Sign in with Linkedin
-        </button>
+    <div className="container">
+      <div className="card hero">
+        <p className="text-display-1 hero-title">
+          Try Linkedin <br /> Roast
+        </p>
+        <p className="text-body-1 hero-tagline">Sign In with your LinkedIn</p>
+
+        <LoginLink className="btn btn-light btn-big">Sign In</LoginLink>
       </div>
-    </main>
+    </div>
   );
 }
