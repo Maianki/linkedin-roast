0aec683d120ac07729fa882fdd461930b1f4912b
Ankit Kumain <kumankit10@gmail.com>
2024-03-10 20:40:50 +0530

    add twitter card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,18 @@ import {
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 import { Toaster } from "react-hot-toast";
-
 export const metadata = {
   title: "LinkedIn Roast",
   description: "Get your linkedin profile roasted!",
+  twitter: {
+    card: "summary_large_image",
+    title: "LinkedIn Roast",
+    description:
+      "Ready to turn your LinkedIn profile into the hottest topic on the internet? 🔥 Get roasted and toasted with hilarious feedback that'll make your profile stand out like never before!",
+    images: [
+      "https://github.com/Maianki/linkedin-roast/assets/28630412/e0fd8cec-24c6-42a5-8589-c65f9780efe8",
+    ], // Must be an absolute URL
+  },
 };
 
 export default async function RootLayout({
