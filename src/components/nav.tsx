c1084b09f4a097f0d4e9f41b7f6e2b535fdb3173
Ankit Kumain <kumankit10@gmail.com>
2024-03-12 21:51:00 +0530

    wip

---
+++ src/components/nav.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { signInWithLinkedIn, signOut } from "@/app/utils/supabase/linkedinAuth";
+
+export default function Navbar({ data }: { data: any }) {
+  const user = data?.user?.user_metadata;
+  return (
+    <nav className="nav container">
+      <h1 className="text-display-3">LinkedIn Roast</h1>
+      <div>
+        {!data?.user ? (
+          <>
+            <a
+              role="button"
+              className="btn btn-ghost sign-in-btn cursor-pointer"
+              onClick={signInWithLinkedIn}
+            >
+              Sign in
+            </a>
+          </>
+        ) : (
+          <div className="profile-blob">
+            {user?.picture ? (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                className="avatar"
+                src={user?.picture}
+                alt="user profile avatar"
+                referrerPolicy="no-referrer"
+              />
+            ) : (
+              <div className="avatar">
+                {user?.given_name?.[0]}
+                {user?.family_name?.[0]}
+              </div>
+            )}
+            <div>
+              <p className="text-heading-2">
+                {user?.given_name} {user?.family_name}
+              </p>
+
+              <a role="button" className="text-subtle cursor-pointer">
+                Log out
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,19 @@
-import {
-  authMiddleware,
-  withAuth,
-} from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { type NextRequest } from "next/server";
+import { updateSession } from "./app/utils/supabase/middleware";
 
-export default function middleware(req: Request) {
-  return withAuth(req);
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
