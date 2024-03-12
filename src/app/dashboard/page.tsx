89979d94ce892bb3284dc8a34fff947233b507d9
Ankit Kumain <kumankit10@gmail.com>
2024-03-12 23:49:54 +0530

    migrating to supbase linkedin auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,43 +4,54 @@ import toast from "react-hot-toast";
 import { useEffect, useState } from "react";
 import { ColorRing } from "react-loader-spinner";
 import { createClient } from "../utils/supabase/client";
+import { User, UserMetadata } from "@supabase/supabase-js";
+
+// Adjust the type annotation for UserResponse
+type UserResponse = {
+  user: User | null;
+  error: Error | null;
+};
 
 export default function Dashboard() {
   const [audioSrc, setAudioSrc] = useState<string>("");
   const [isRoast, setIsRoast] = useState<boolean>(false);
   const [title, seTitle] = useState("");
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<UserMetadata | undefined>(undefined);
 
   const supabase = createClient();
 
   useEffect(() => {
-    (async function () {
-      const data = await supabase.auth.getUser();
-      console.log(data);
-
-      setUser(data);
-    })();
+    try {
+      (async function () {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+        setUser(user?.user_metadata);
+      })();
+    } catch (err) {
+      toast.error("Something went wrong! Try refreshing the page");
+    }
   }, []);
 
-  // const handleRoast = async () => {
-  //   setIsRoast(true);
-  //   try {
-  //     const audioBuffer = await getRoast(
-  //       user?.picture as string,
-  //       `${user?.given_name}`,
-  //       title
-  //     );
-  //     const audioBlob = new Blob([audioBuffer as unknown as string], {
-  //       type: "audio/mpeg",
-  //     });
-  //     setAudioSrc(URL.createObjectURL(audioBlob));
-  //     setIsRoast(false);
-  //   } catch (err) {
-  //     setIsRoast(false);
-  //     toast("Something went wrong! Try again");
-  //     console.log("Something went wrong!");
-  //   }
-  // };
+  const handleRoast = async () => {
+    setIsRoast(true);
+    try {
+      const audioBuffer = await getRoast(
+        user?.picture as string,
+        `${user?.given_name}`,
+        title
+      );
+      const audioBlob = new Blob([audioBuffer as unknown as string], {
+        type: "audio/mpeg",
+      });
+      setAudioSrc(URL.createObjectURL(audioBlob));
+      setIsRoast(false);
+    } catch (err) {
+      setIsRoast(false);
+      toast.error("Something went wrong! Try again");
+      console.log("Something went wrong!");
+    }
+  };
 
   return (
     <div className="container">
@@ -67,7 +78,7 @@ export default function Dashboard() {
           <button
             disabled={isRoast}
             className="btn btn-light btn-med cursor-pointer"
-            onClick={() => {}}
+            onClick={handleRoast}
             style={{
               display: "flex",
               alignItems: "center",
